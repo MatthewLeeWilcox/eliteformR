f18ec496c7a83adfd7c03ffe39b4d31ea395e30e
MatthewLeeWilcox <matthewleewilcox@gmail.com>
2025-08-08 21:02:54 -0400

    adjust_description

--- R/classes.R
+++ R/classes.R
@@ -1,3 +1,13 @@
+#' API_TOKEN S4 class
+#'
+#' Holds EliteForm API credentials/metadata.
+#'
+#' @slot url Character scalar with the EliteForm base URL.
+#' @slot api_key Character scalar with the X-ApiKey value.
+#' @slot TEAM_ID Character (typically length-1) team identifier.
+#'
+#' @exportClass API_TOKEN
+#' @keywords classes
 setClass("API_TOKEN", slots = list(
   url = "character",
   api_key = "character",
